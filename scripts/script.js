461c9069f40680b9b281ac67e8d38f66d3cb7409
yingyunhu <112943865+yingyunhu@users.noreply.github.com>
2022-09-22 10:36:25 +0200

    voortgang 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,8 @@
-// JavaScript Document
-console.log("hi");
+var menuButton = document.querySelector("button");
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	var menu = document.querySelector("nav:nth-of-type(2)");
+	menu.classList.toggle("open");
+}
